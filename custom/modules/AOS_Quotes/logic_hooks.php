9b652fb20064d33d951c9c308ea8891b7b6870c1
Luis Carlos Osorio Jayk <luiscarlosjayk@gmail.com>
2014-05-05 16:58:52 -0400

    Updating more custom files

---
+++ custom/modules/AOS_Quotes/logic_hooks.php
@@ -0,0 +1,23 @@
+<?php
+
+$hook_version = 1;
+$hook_array = array();
+$hook_array['before_save'] = array();
+$hook_array['before_save'][] = array(
+    1,
+    'Unique Oustanding Quote',
+    'custom/udd/hooks/modules/AOS_Quotes/QuotesHooks.php',
+    'QuotesHooks',
+    'before_save'
+);
+
+$hook_array['after_save'] = array();
+$hook_array['after_save'][] = array(
+    1,
+    'Unique Oustanding Quote',
+    'custom/udd/hooks/modules/AOS_Quotes/QuotesHooks.php',
+    'QuotesHooks',
+    'after_save'
+);
+
+?>
---
+++ custom/modules/Meetings/Ext/Vardefs/vardefs.ext.php
@@ -0,0 +1,28 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+
+$dictionary['Meeting']['fields']['name']['name'] = 'name';
+$dictionary['Meeting']['fields']['name']['type'] = 'enum';
+$dictionary['Meeting']['fields']['name']['options'] = 'meetings_name_list_c';
+
+ 
+
+
+ // created: 2014-04-06 14:34:24
+
+ 
+
+ // created: 2014-04-06 14:34:25
+
+ 
+
+ // created: 2014-04-06 14:34:25
+
+ 
+
+ // created: 2014-04-06 14:34:25
+
+ 
+?>
