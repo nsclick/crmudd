6481028a4993c5de311ca2ed6a5181838c4c51ff
Luis Carlos Osorio Jayk <luiscarlosjayk@gmail.com>
2014-05-06 14:35:07 -0400

    Added Opportunities Menu Items

---
+++ custom/Extension/modules/Opportunities/Ext/Menus/CustomMenu.php
@@ -0,0 +1,22 @@
+<?php
+global $mod_strings, $app_strings, $sugar_config;
+
+if(ACLController::checkAccess('Opportunities','edit',true)) {
+	$module_menu[]=  array(
+		"index.php?entryPoint=executivePanel",
+		"Panel de Asignaci&oacute;n de Oportunidades",
+		"Import"
+	);
+
+}
+
+if(ACLController::checkAccess('Opportunities','delete',true)) {
+	$module_menu[]=  array(
+		"index.php?entryPoint=administracion",
+		"Panel de Administraci&oacute;n",
+		"Import"
+	);
+
+}
+
+?>
